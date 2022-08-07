6aaa7d384e9d83e85f0feaa5d548d1ed336cd63f
Elgiz Abbasov <lgzabbasov@gmail.com>
2022-08-06 22:48:44 -0600

    testing heroku media host

--- kicksale/settings/core.py
+++ kicksale/settings/core.py
@@ -121,7 +121,7 @@ STATICFILES_DIRS = (
 )
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Cart session ID
 CART_SESSION_ID = 'cart'
